03e314329a9db5fddfbbad490e0cbd25847c7f66
Urbancareai <visualvoice.ai@gmail.com>
2025-06-16 21:42:20 -0400

    Initail design system is done in frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import DesignSystem from './pages/DesignSystem'
+import './styles/globals.css'
+import './styles/components.css'
+
+function App() {
+  return <DesignSystem />
+}
+
+export default App
